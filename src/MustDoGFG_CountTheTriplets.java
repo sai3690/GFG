5043242c31e88853d38a4b9b36ca295ffa887cf2
sai3690 <saisamarpan2012@gmail.com>
2022-06-22 23:59:54 +0530

    GFG Must Do: Count the triplets

--- src/MustDoGFG_CountTheTriplets.java
+++ src/MustDoGFG_CountTheTriplets.java
@@ -6,7 +6,7 @@ public class MustDoGFG_CountTheTriplets {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of element of the array");
+        System.out.println("Enter the number of element of the arra");
 
         int n = sc.nextInt();
 
